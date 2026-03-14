2cc640ba239a085d14ef7373c35f36164016a1c6
ruslanhamidov <rhamidov202@gmail.com>
2026-03-14 14:14:46 -0700

    cors update

--- main.py
+++ main.py
@@ -12,7 +12,7 @@ app = FastAPI(title="GapHack API")
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173", "http://127.0.0.1:5173", "https://gaphack-frontend.vercel.app/"],
+    allow_origins=["http://localhost:5173", "http://127.0.0.1:5173", "https://gaphack-frontend.vercel.app"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
